827f6c3017cb3accb4717fee01c00ecf4ea5f2dd
Yokoyama Shoichi <yokoyama.s.ae@is-mac12.edu.c.titech.ac.jp>
2019-06-25 16:31:42 +0900

    分からない

---
+++ sort/src/quick_sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+// 素数
+#define N 2999
+
+int A[N];
+
+// *p と *q の値を入れ替える関数
+void swap(int *p, int *q){
+  int tmp;
+  tmp = *p;
+  *p = *q;
+  *q = tmp;
+}
+
+
+/*
+A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
+*/
+void quick_sort(int A[], int n){
+    int i, j, pivot;
+    
+    pivot = A[n/2];
+    for(i = j = 0; i < n; i++){
+        if(A[i] <= pivot){
+            swap(A[i], A[n-1-j]);
+            j++;
+        }
+    }
+    int B[N];
+    for(i = 0; i < n-j; i++){
+        B[i] = A[j+i];
+    }
+    quick_sort(B[n-j-1], n-j);
+    quick_sort(A[j-1], j);
+    
+
+}
+
+int main(){
+  int i;
+  A[0] = 0;
+  A[1] = 17; //原始元
+  for(i=2;i<N;i++){
+    A[i] = (long long int) A[i-1] * A[1] % N;
+  }
+
+  quick_sort(A, N);
+  for(i=0;i<N;i++){
+    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+  }
+}
